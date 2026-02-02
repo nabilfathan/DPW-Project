f8739f2aafd8a80cd3485aa3278fdb0cbbabbdd6
nabilfathan <akunmiliksaya10@gmail.com>
2026-02-02 14:41:40 +0700

    test2

--- index.php
+++ index.php
@@ -3,3 +3,16 @@
 ?>
 
 <!DOCTYPE html>
+<header>
+    <title>My Website</title>
+</header>
+
+<body>
+<h1>Welcome to My Website</h1>
+<p>This is the homepage of my website.</p>
+</body>
+
+<footer>
+    <hr>
+    <p>&copy; 2024 My Website</p>
+</footer>
