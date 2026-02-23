0cd5d519fd4d994fbb9abef5eddbf80e6eb24949
nabilfathan <akunmiliksaya10@gmail.com>
2026-02-23 22:31:56 +0700

    Update web

---
+++ aktivitas/tugas_js.js
@@ -0,0 +1,9 @@
+function munculOtomatis() {
+    let nama = prompt("Halaman ini menyatakan:\nTuliskan nama Kamu");
+
+    if (nama !== null && nama !== "") {
+        alert("Nama saya " + nama + ", saya akan mengamalkan Pancasila dan UUD 1945 sebagai Dasar Negara.");
+    }
+}
+
+munculOtomatis();
