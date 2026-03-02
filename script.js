9cd51cb32783f28a58652074cbf13be23b88dd09
nabilfathan <akunmiliksaya10@gmail.com>
2026-03-02 14:29:18 +0700

    Update web

---
+++ script.js
@@ -0,0 +1,20 @@
+const countdownTimer = {
+    targetDate: new Date("Feb 29, 2090 10:00:00").getTime(),
+
+    update: function() {
+        const now = new Date().getTime();
+        const distance = this.targetDate - now;
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById("days").innerText = days.toString().padStart(2, '0');
+        document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+        document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+        document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0'); 
+    }
+}
+
+setInterval(() => {
+    countdownTimer.update();
+}, 1000);
